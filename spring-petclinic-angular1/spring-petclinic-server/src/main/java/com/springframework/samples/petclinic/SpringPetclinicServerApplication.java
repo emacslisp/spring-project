88d9d406a46da0009414f6ee4545bdcec91475a6
Eddy Wu <ewu@echoice.com.au>
2017-12-11 15:46:25 +1100

    finish project create

---
+++ spring-petclinic-angular1/spring-petclinic-server/src/main/java/com/springframework/samples/petclinic/SpringPetclinicServerApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.samples.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetclinicServerApplication.class, args);
+	}
+}
