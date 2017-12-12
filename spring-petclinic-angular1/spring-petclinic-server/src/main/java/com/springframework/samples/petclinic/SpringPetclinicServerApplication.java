d9a588d4e014fbb4dd566335f9b3c6e4e70049f4
emacslisp <java.gdb@gmail.com>
2017-12-12 13:53:14 +1100

    ===add pom config file===

--- spring-petclinic-angular1/spring-petclinic-server/src/main/java/com/springframework/samples/petclinic/SpringPetclinicServerApplication.java
+++ spring-petclinic-angular1/spring-petclinic-server/src/main/java/com/springframework/samples/petclinic/SpringPetclinicServerApplication.java
@@ -2,8 +2,17 @@ package com.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.samples.petclinic.config.PetclinicProperties;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PetclinicProperties.class)
 public class SpringPetclinicServerApplication {
 
 	public static void main(String[] args) {
