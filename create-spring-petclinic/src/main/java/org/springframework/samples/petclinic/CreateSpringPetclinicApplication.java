13f622acb00301d20a2931105c2a06d4e2392f9c
emacslisp <java.gdb@gmail.com>
2017-12-13 09:42:40 +1100

    === add spring-petclinic with spring and html ===

---
+++ create-spring-petclinic/src/main/java/org/springframework/samples/petclinic/CreateSpringPetclinicApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateSpringPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateSpringPetclinicApplication.class, args);
+	}
+}
