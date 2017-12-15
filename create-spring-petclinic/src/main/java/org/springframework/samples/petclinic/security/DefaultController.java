e91578cb6f34880085131bded6d07b530ed88fd1
emacslisp <java.gdb@gmail.com>
2017-12-15 11:27:32 +1100

    finish spring security integration with spring pet clinic

---
+++ create-spring-petclinic/src/main/java/org/springframework/samples/petclinic/security/DefaultController.java
@@ -0,0 +1,20 @@
+package org.springframework.samples.petclinic.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
+
+}
+
