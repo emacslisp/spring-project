4ca841867e4d118b443a3838a7518f0d82a25889
emacslisp <java.gdb@gmail.com>
2017-12-22 23:52:59 +1100

    finish inner bean

--- lab/spring-tutorial-15/src/main/java/com/lab/springscratch/App.java
+++ lab/spring-tutorial-15/src/main/java/com/lab/springscratch/App.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans-16.xml");
         
         Jungle jungle = (Jungle)context.getBean("jungle");
         
