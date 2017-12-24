ef063b712b1f8482ed4bfee43bee2b5f3a9041aa
emacslisp <java.gdb@gmail.com>
2017-12-24 22:42:32 +1100

    finish autowire by constructor

--- lab/spring-tutorial-19/src/main/java/com/lab/springscratch/Logger.java
+++ lab/spring-tutorial-19/src/main/java/com/lab/springscratch/Logger.java
@@ -3,6 +3,12 @@ package com.lab.springscratch;
 public class Logger {
 	private ConsoleWriter consoleWriter;
 	private FileWriter fileWriter;
+	
+	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+		this.consoleWriter = consoleWriter;
+		this.fileWriter = fileWriter;
+		System.out.println("constructor are called");
+	}
 
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
