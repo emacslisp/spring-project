ccf5c5e61fbdbc8ddc1062f8d3ee14d175f24812
emacslisp <java.gdb@gmail.com>
2017-12-24 21:26:35 +1100

    Finish autowire

---
+++ lab/spring-tutorial-19/src/main/java/com/lab/springscratch/Logger.java
@@ -0,0 +1,22 @@
+package com.lab.springscratch;
+
+public class Logger {
+	private ConsoleWriter consoleWriter;
+	private FileWriter fileWriter;
+
+	public void setConsoleWriter(ConsoleWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	public void setFileWriter(FileWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		consoleWriter.write(text);
+	}
+}
