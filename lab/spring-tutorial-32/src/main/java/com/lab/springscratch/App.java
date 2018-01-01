d246dcc78c9c7cad6b3e411dd58c5c0a3fc12285
emacslisp <java.gdb@gmail.com>
2018-01-01 22:31:03 +1100

    finish @Value test

---
+++ lab/spring-tutorial-32/src/main/java/com/lab/springscratch/App.java
@@ -0,0 +1,18 @@
+package com.lab.springscratch;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		Robot robot = (Robot)context.getBean("robot");
+		
+		robot.speak();
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
