c5898cda3a68400a65fea15877ecb183324237da
emacslisp <java.gdb@gmail.com>
2018-01-06 00:40:01 +1100

    update exception

--- lab/spring-tutorial-40/src/main/java/com/lab/springscratch/App.java
+++ lab/spring-tutorial-40/src/main/java/com/lab/springscratch/App.java
@@ -4,22 +4,26 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.dao.DataAccessException;
 
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		
-		OffersDAO offersDao = (OffersDAO)context.getBean("offersDao");
-		
-		List<Offer> offers = offersDao.getOffers();
-		
-		for(Offer offer: offers) {
-			System.out.println(offer);
+
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		try {
+			List<Offer> offers = offersDao.getOffers();
+
+			for (Offer offer : offers) {
+				System.out.println(offer);
+			}
+		} catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
 		}
-		
-		((ClassPathXmlApplicationContext)context).close();
+
+		((ClassPathXmlApplicationContext) context).close();
 	}
 }
