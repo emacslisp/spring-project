11f0f0d12a8bb3e2d953338e026ffdafdf0ac849
emacslisp <java.gdb@gmail.com>
2018-01-06 12:44:40 +1100

    Map Sql Parameter

--- lab/spring-tutorial-40/src/main/java/com/lab/springscratch/OffersDAO.java
+++ lab/spring-tutorial-40/src/main/java/com/lab/springscratch/OffersDAO.java
@@ -9,21 +9,27 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component("offersDao")
 public class OffersDAO {
-	private JdbcTemplate jdbc;
+	private NamedParameterJdbcTemplate jdbc;
 	
 	@Autowired
 	public void setDataSource(DataSource jdbc) {
-		this.jdbc = new JdbcTemplate(jdbc);
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
 		System.out.println("============ init data source =============");
 	}
 
 	public List<Offer> getOffers() {
 		
-		return jdbc.query("select * from offers", new RowMapper<Offer>() {
+		//MapSqlParameterSource params = new MapSqlParameterSource("name","Sue");
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("name", "Sue");
+		
+		return jdbc.query("select * from offers where name = :name", params, new RowMapper<Offer>() {
 
 			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
 				Offer offer = new Offer();
