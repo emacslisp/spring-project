9910da66abeb0bf8e666d5c9e95c6b79d13bbadb
emacslisp <java.gdb@gmail.com>
2018-01-07 18:04:35 +1100

    finish intergration of spring with tomcat

---
+++ lab/spring-tutorial-50/src/com/lab/spring/controllers/OffersController.java
@@ -0,0 +1,13 @@
+package com.lab.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController {
+
+	@RequestMapping("/")
+	public String showHome() {
+		return "Home";
+	}
+}
