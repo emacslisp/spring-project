8f6250d52102d47433a05d57a40c6cd290833fe6
emacslisp <java.gdb@gmail.com>
2018-01-12 20:25:29 +1100

    add validation

--- lab/spring-tutorial-50/src/com/lab/spring/controllers/OffersController.java
+++ lab/spring-tutorial-50/src/com/lab/spring/controllers/OffersController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -57,9 +60,22 @@ public class OffersController {
 	}
 	
 	@RequestMapping(value="/docreate", method = RequestMethod.POST)
-	public String doCreate(Model model, Offer offer) {
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
 		// this means index.jsp, "xxx".jsp
-		System.out.println(offer);
+		//System.out.println(offer);
+		if(result.hasErrors()) {
+			System.out.println("form is not valid");
+			
+			List<ObjectError> errors = result.getAllErrors();
+			
+			for(ObjectError error: errors) {
+				System.out.println(error);
+			}
+		}
+		else {
+			System.out.println("Form Valid");
+		}
+		
 		return "offercreated";
 	}
 }
