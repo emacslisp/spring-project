763751a39359581cda53ac4d62c601691795952f
emacslisp <java.gdb@gmail.com>
2018-01-13 17:24:08 +1100

    update AOP

---
+++ lab/spring-tutorial-76/src/main/java/com/lab/spring/SpringTutorial76Application.java
@@ -0,0 +1,12 @@
+package com.lab.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorial76Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorial76Application.class, args);
+	}
+}
