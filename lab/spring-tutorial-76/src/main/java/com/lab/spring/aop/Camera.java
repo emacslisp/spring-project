d88df0ae7f746f89e8b4d28a57928da5241af498
emacslisp <java.gdb@gmail.com>
2018-01-14 12:23:44 +1100

    add aop annotaion

--- lab/spring-tutorial-76/src/main/java/com/lab/spring/aop/Camera.java
+++ lab/spring-tutorial-76/src/main/java/com/lab/spring/aop/Camera.java
@@ -1,5 +1,10 @@
 package com.lab.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class Camera {
 	public void snap() {
 		System.out.println("SNAP!");
--- lab/spring-tutorial-76/src/main/java/com/lab/spring/aop/Logger.java
+++ lab/spring-tutorial-76/src/main/java/com/lab/spring/aop/Logger.java
@@ -1,6 +1,19 @@
 package com.lab.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class Logger {
+	
+	@Pointcut("execution(void com.lab.spring.aop.Camera.snap())")
+	public void cameraSnap() {
+	}
+	
+	@Before("cameraSnap()")
 	public void aboutToTakePhoto() {
 		System.out.println("About to take photo...");
 	}
