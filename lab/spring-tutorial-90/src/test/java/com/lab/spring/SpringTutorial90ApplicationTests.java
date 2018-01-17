72e7d61d81f6680576000801e1c0b18fa3c51678
emacslisp <java.gdb@gmail.com>
2018-01-17 11:38:53 +1100

    add project for Aspects Functionality

---
+++ lab/spring-tutorial-90/src/test/java/com/lab/spring/SpringTutorial90ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lab.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTutorial90ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
